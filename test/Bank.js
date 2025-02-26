999d328f4f1845fb5528fb0033d40f231b15800a
carl <carl.shen@apple.com>
2025-02-26 18:05:11 +0800

    测试提交PR自动执行测试用例

--- test/Bank.js
+++ test/Bank.js
@@ -20,21 +20,18 @@ describe("TestBankContract", function () {
   // 测试存款功能
   it("should allow deposits and track balances", async function () {
     const depositValue = ethers.utils.parseEther("1.0");
-    const tx = await connectAddr1.deposit({ value: depositValue });
+    const tx =  await connectAddr1.deposit({ value: depositValue });
     await tx.wait();
     const finalDepositValue = await bank.deposits(addr1.address);
-    finalDepositValue = finalDepositValue;
     expect(finalDepositValue).to.equal(depositValue);
-
   });
 
   // 测试取款功能
   it("should allow withdrawals and reset balances", async function () {
-
     const initBalance = await addr1.getBalance();
     const tx = await connectAddr1.withdraw();
     const receipt = await tx.wait();
-    const { gasUsed } = receipt;
+    const{gasUsed} = receipt;
     const txCost = gasUsed.mul(receipt.effectiveGasPrice)
 
     const finalBalance = await addr1.getBalance();
@@ -44,6 +41,7 @@ describe("TestBankContract", function () {
     expect(await bank.deposits(addr1.address)).to.be.equal("0");
 
   });
+
 
 });
 
